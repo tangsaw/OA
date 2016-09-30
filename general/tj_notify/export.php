e714b66d0dda76413429a222ab4ec90751bf5da1
tangsaw <xxhb@tangsaw.com.cn>
2016-09-30 13:42:56 +0800

    v1.0
    新增导出程序

---
+++ general/tj_notify/export.php
@@ -0,0 +1,57 @@
+<?
+include_once("inc/auth.inc.php");
+include_once("inc/header.inc.php");
+include_once("inc/utility_all.php");
+$CUR_DATE=date("Y-m-d",time());
+
+$EXCEL_OUT=array(_("序号"),_("日期"),_("公司"),_("部门"),_("员工工号"),_("员工姓名"));
+
+require_once ('inc/ExcelWriter.php');
+
+$objExcel = new ExcelWriter();
+$objExcel->setFileName(_("未提交工作日志统计结果".$CUR_DATE));
+$objExcel->addHead($EXCEL_OUT);
+
+
+$CONDITION_STR = " select * from td_oa.ud_usernodiary where 1=1 ";
+if($USER_DEPT_QRY != "") {
+    $CONDITION_STR.="and user_Dept='".$USER_DEPT_QRY."' ";
+}
+if($TYPE_QRY != "") {
+    //$CONDITION_STR.="and type='".$TYPE_QRY."' ";
+}
+if($BEGIN_DATE_QRY != "") {
+    $CONDITION_STR.="and t_Date>='".$BEGIN_DATE_QRY."' ";
+}
+if($END_DATE_QRY != "") {
+    $CONDITION_STR.="and t_Date<='".$END_DATE_QRY."' ";
+}
+if($COPY_TO_ID_QRY != "") {
+    $CONDITION_STR.="and USER_ID ='".$COPY_TO_ID_QRY."' ";
+}
+if($BEGIN_DATE_QRY == "" & $END_DATE_QRY == "" & $COPY_TO_ID_QRY == "") {
+    $CONDITION_STR = " select * from td_oa.ud_usernodiary where 1 > 1 ";
+    Message("没有输入查询条件，请重新输入！");
+    Button_Back();
+    exit;
+}
+$query = $CONDITION_STR." order by t_Date desc, DEPT_ID asc";
+$cursor = exequery(TD::conn(),$query);
+$WORK_PLAN_COUNT=0;
+while($ROW=mysql_fetch_array($cursor))
+{
+    $WORK_PLAN_COUNT++;
+    $SRL_NO=$WORK_PLAN_COUNT;
+    $DATE=$ROW["t_Date"];
+    $COMPANY=$ROW["user_Dept"];
+    $DEPTNAME=$ROW["DEPT_NAME"];
+    $EMPNO=$ROW["BYNAME"];
+    $EMPNAME=$ROW["USER_NAME"];
+
+    
+   $EXCEL_OUT="$SRL_NO,$DATE,$COMPANY,$DEPTNAME,$EMPNO,$EMPNAME";
+   $objExcel->addRow($EXCEL_OUT);
+}
+
+$objExcel->Save();
+?>
--- general/tj_notify/index.php
+++ general/tj_notify/index.php
@@ -14,15 +14,43 @@ include_once("inc/header.inc.php");
 <form enctype="multipart/form-data" action="index.php"  method="post" name="form1">
 <table border="0" width="95%" align="center" cellspacing="0" cellpadding="3" class="big" >
   <tr>
-      <td nowrap class="TableData" widht="20%" nowrap>  <?=_("开始日期：")?><input type="text" name="BEGIN_DATE" size="10" maxlength="10" id="start_time" class="BigInput" value="<?=$BEGIN_DATE?>" onClick="WdatePicker()"></td>
-      <td class="TableData" widht="20%" nowrap><?=_("结束日期：")?><input type="text" name="END_DATE" size="10" maxlength="10" class="BigInput" value="<?=$END_DATE?>" onClick="WdatePicker({minDate:'#F{$dp.$D(\'start_time\')}'})"></td>
-      <td nowrap class="TableData" widht="20%" nowrap><?=_("人员：")?>
-          <input type="hidden" name="COPY_TO_ID" value="<?=$COPY_TO_ID?>">
-          <input type="text" name="COPY_TO_NAME" size="20" class="BigStatic" readonly value="<?=$COPY_TO_NAME?>">
-          <a href="javascript:;" class="orgAdd" onClick="SelectUserSingle('96','','COPY_TO_ID', 'COPY_TO_NAME')"><?=_("添加")?></a>
+      <td>
+          <?=_("公司：")?>
+          <select name="USER_DEPT" class="BigSelect">
+              <?
+              $query = "SELECT DISTINCT(DEPT_NAME), DEPT_NO from department where dept_parent = 0 order by DEPT_NO ASC ";
+              $cursor= exequery(TD::conn(),$query);
+              while($ROW=mysql_fetch_array($cursor))
+              {
+                  $DEPT_NAME=$ROW["DEPT_NAME"];
+                  ?>
+                  <option value="<?=$DEPT_NAME?>" <?if($USER_DEPT==$DEPT_NAME) echo "selected";?>><?=$DEPT_NAME?></option>
+                  <?
+              }
+              ?>
+          </select>
       </td>
-      <td nowrap>
-          <input type="submit" value="<?=_("查询")?>" class="BigButton" align="left">
+      <td>
+          <?=_("类型：")?>
+          <select name="TYPE" class="BigSelect">
+              <option value="0" <?if($TYPE=="0") echo "selected";?>><?=_("工作日志")?></option>
+              <option value="1" <?if($TYPE=="1") echo "selected";?>><?=_("月工作总结")?></option>
+              <option value="2" <?if($TYPE=="2") echo "selected";?>><?=_("季度工作总结")?></option>
+              <option value="3" <?if($TYPE=="3") echo "selected";?>><?=_("年度工作总结")?></option>
+          </select>
+      </td>
+      <td>
+            <?=_("开始日期：")?><input type="text" name="BEGIN_DATE" size="10" maxlength="10" id="start_time" class="BigInput" value="<?=$BEGIN_DATE?>" onClick="WdatePicker()">
+            ~<?=_("结束日期：")?><input type="text" name="END_DATE" size="10" maxlength="10" class="BigInput" value="<?=$END_DATE?>" onClick="WdatePicker({minDate:'#F{$dp.$D(\'start_time\')}'})">
+      </td>
+      <td ><?=_("人员：")?>
+          <input type="text" name="COPY_TO_NAME" size="10" class="BigStatic" readonly value="<?=$COPY_TO_NAME?>">
+          <a href="javascript:;" class="orgAdd" onClick="SelectUserSingle('96','','COPY_TO_ID', 'COPY_TO_NAME')"><?=_("添加")?></a>
+          <input type="hidden" name="COPY_TO_ID" value="<?=$COPY_TO_ID?>">
+      </td>
+      <td>
+          <input type="submit" value="<?=_("查询")?>" class="BigButton" >
+          <input type="button"  value="<?=_("导出")?>" class="BigButton" onClick="exportReport()" title="<?=_("导出统计结果")?>">
       </td>
   </tr>
 </table>
@@ -33,26 +61,34 @@ include_once("inc/header.inc.php");
     <tr class="TableHeader">
         <td nowrap align="center" width="5%"><?=_("序号")?></td>
         <td nowrap align="center" width="15%"><?=_("日期")?></td>
-        <td nowrap align="center" width="30%"><?=_("部门")?></td>
-        <td nowrap align="center" width="25%"><?=_("工号")?></td>
-        <td nowrap align="center" width="25%"><?=_("姓名")?></td>
+        <td nowrap align="center" width="20%"><?=_("公司")?></td>
+        <td nowrap align="center" width="20%"><?=_("部门")?></td>
+        <td nowrap align="center" width="20%"><?=_("工号")?></td>
+        <td nowrap align="center" width="20%"><?=_("姓名")?></td>
     </tr>
     <?
     //============================ 显示查询结果 =======================================
     $CONDITION_STR = " select * from td_oa.ud_usernodiary where 1=1 ";
+    if($USER_DEPT != "") {
+        $CONDITION_STR.="and user_Dept='".$USER_DEPT."' ";
+    }
+    if($TYPE != "") {
+        //$CONDITION_STR.="and type='".$TYPE."' ";
+    }
     if($BEGIN_DATE != "") {
-        $CONDITION_STR.="and ADDDATE>='".$BEGIN_DATE."' ";
+        $CONDITION_STR.="and t_Date>='".$BEGIN_DATE."' ";
     }
     if($END_DATE != "") {
-        $CONDITION_STR.="and ADDDATE<='".$END_DATE."' ";
+        $CONDITION_STR.="and t_Date<='".$END_DATE."' ";
     }
     if($COPY_TO_ID != "") {
         $CONDITION_STR.="and USER_ID ='".$COPY_TO_ID."' ";
     }
     if($BEGIN_DATE == "" & $END_DATE == "" & $COPY_TO_ID == "") {
         $CONDITION_STR = " select * from td_oa.ud_usernodiary where 1 > 1 ";
+        echo "<font color='red'>&nbsp;&nbsp;请输入查询条件,日期与人员至少输入一项！</font>";
     }
-    $query = $CONDITION_STR." order by t_Date desc, DEPT_ID desc";
+    $query = $CONDITION_STR." order by t_Date desc, DEPT_ID asc";
     $cursor= exequery(TD::conn(),$query);
     $WORK_PLAN_COUNT=0;
     while($ROW=mysql_fetch_array($cursor))
@@ -60,6 +96,7 @@ include_once("inc/header.inc.php");
         $WORK_PLAN_COUNT++;
         $SRL_NO=$WORK_PLAN_COUNT;
         $DATE=$ROW["t_Date"];
+        $USER_DEPT=$ROW["user_Dept"];
         $DEPTNAME=$ROW["DEPT_NAME"];
         $EMPNO=$ROW["BYNAME"];
         $EMPNAME=$ROW["USER_NAME"];
@@ -73,6 +110,7 @@ include_once("inc/header.inc.php");
     <tr class="<?=$TableLine?>">
         <td align="center"><?=$SRL_NO?></td>
         <td align="center"><?=$DATE?></td>
+        <td align="center"><?=$USER_DEPT?></td>
         <td align="center"><?=$DEPTNAME?></td>
         <td align="center"><?=$EMPNO?></td>
         <td align="center"><?=$EMPNAME?></td>
@@ -81,3 +119,19 @@ include_once("inc/header.inc.php");
     }
 ?>
 </table>
+<script>
+    function exportReport() {
+        if (form1.BEGIN_DATE.value == ""
+            && form1.END_DATE.value == ""
+            && form1.COPY_TO_ID.value == "") {
+            alert("请输入导出条件！");
+            return;
+        }
+        location.href='export.php?BEGIN_DATE_QRY='+form1.BEGIN_DATE.value
+            +"&END_DATE_QRY="+form1.END_DATE.value
+            +"&COPY_TO_ID_QRY="+form1.COPY_TO_ID.value
+            +"&TYPE_QRY="+form1.TYPE.value
+            +"&USER_DEPT_QRY="+form1.USER_DEPT.value;
+    }
+
+</script>
